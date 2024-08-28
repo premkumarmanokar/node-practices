3e9dd61744d8d8be87a352c1b5f4d07d7d9adf7d
premkumarmanokar <premkumarvpm1699@gmail.com>
2024-08-29 02:29:39 +0530

    fs manipulation

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 //importing the express module
 const express = require("express");
-
+const fs = require("fs");
 //create an express application
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("GET world!");
+app.get("/stats", (req, res) => {
+  fs.stat("./files/test.txt", (err, stats) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json({
+        size: `${stats.size} bytes`,
+        isFile: stats.isFile(),
+        isDirectory: stats.isDirectory(),
+        isSymbolicLink: stats.isSymbolicLink(),
+      });
+    }
+  });
 });
 
-app.post("/", (req, res) => {
-  res.send("POST world!");
+app.post("/create", (req, res) => {
+  fs.writeFile("./files/newfile.txt", "Hello world!", (err) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send("File created successfully");
+    }
+  });
 });
 
-app.get("/test", (req, res) => {
-  res.send("GET test!");
+app.get("/read", (req, res) => {
+  fs.readFile("./files/test.txt", "utf8", (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(data);
+    }
+  });
 });
 
 //starting the server by listening on a port for incoming requests
